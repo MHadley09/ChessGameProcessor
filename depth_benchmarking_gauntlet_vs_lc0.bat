de5ab8240380c1a15c5be27458752d754af4d439
Michael <michael.hadley09@gmail.com>
2026-03-14 11:41:03 -0700

    Updating to add a baseline
    Upping game count as well for a longer run for more accurate assessment

--- depth_benchmarking_gauntlet_vs_lc0.bat
+++ depth_benchmarking_gauntlet_vs_lc0.bat
@@ -3,10 +3,11 @@ SET outfile="out_benchmark.txt"
 echo Cutechess started at %date% %time%. Output is redirected to %outfile%
 echo Cutechess started at %date% %time% > %outfile%
 
-cutechess-cli.exe -tournament gauntlet -rounds 25 -games 2 -repeat -concurrency 8 -pgnout out_benchmark.pgn -recover ^
+cutechess-cli.exe -tournament gauntlet -rounds 40 -games 2 -repeat -concurrency 8 -pgnout out_benchmark.pgn -recover ^
 -resign movecount=20 score=10000 -draw movenumber=50 movecount=20 score=20 ^
 -openings file="books/book-ply8-unifen-Q-0.25-0.40.pgn" order=random plies=8 policy=round ^
 -engine name="Lc0" cmd="lc0" ^
+-engine name="Stockfish Baseline" cmd="stockfish-windows-x86-64" ^
 -engine name="Stockfish Depth 28" depth=28 cmd="stockfish-windows-x86-64" ^
 -engine name="Stockfish Depth 24" depth=24 cmd="stockfish-windows-x86-64" ^
 -engine name="Stockfish Depth 20" depth=20 cmd="stockfish-windows-x86-64" ^
