40c5e2974160f76664aa7f5727e8671adbebe610
Michael <michael.hadley09@gmail.com>
2026-03-14 11:31:24 -0700

    Adding benchmarking test file
    We want to justify our selection of depth 12 as a valid default.  Let's run a benchmark and ensure it is still at least ~3000+ FIDE level of performance as anything super-human is sufficient for our eventual training purposes

---
+++ depth_benchmarking_gauntlet_vs_lc0.bat
@@ -0,0 +1,20 @@
+@echo off
+SET outfile="out_benchmark.txt"
+echo Cutechess started at %date% %time%. Output is redirected to %outfile%
+echo Cutechess started at %date% %time% > %outfile%
+
+cutechess-cli.exe -tournament gauntlet -rounds 25 -games 2 -repeat -concurrency 8 -pgnout out_benchmark.pgn -recover ^
+-resign movecount=20 score=10000 -draw movenumber=50 movecount=20 score=20 ^
+-openings file="books/book-ply8-unifen-Q-0.25-0.40.pgn" order=random plies=8 policy=round ^
+-engine name="Lc0" cmd="lc0" ^
+-engine name="Stockfish Depth 28" depth=28 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 24" depth=24 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 20" depth=20 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 16" depth=16 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 14" depth=14 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 12" depth=12 cmd="stockfish-windows-x86-64" ^
+-engine name="Stockfish Depth 8" depth=8 cmd="stockfish-windows-x86-64" ^
+-each proto=uci tc=120+2 >> %outfile%
+echo Tournament ended at %date% %time%.
+echo Tournament ended at %date% %time%. >> %outfile%
+pause
